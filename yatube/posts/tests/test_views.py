2bf89fd566b25d04e6088a33e3e58734441b744a
Владислав Рыбачук <vlad1k-o@yandex.ru>
2022-09-20 01:08:23 +0300

    Edits

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -20,7 +20,7 @@ class PostViewTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user = User.objects.create_user(username='author')
+        cls.user = User.objects.create_user(username='auth')
         cls.author = User.objects.create_user(username='author')
         cls.author_2 = User.objects.create_user(username='author_2')
         cls.group = Group.objects.create(
